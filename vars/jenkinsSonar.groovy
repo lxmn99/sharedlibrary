d697ab00d1c878a60b3909bb6e18f90505242729
lxmn99 <60390751+lxmn99@users.noreply.github.com>
2022-01-25 00:04:58 +0530

    Update jenkinsSonar.groovy

--- vars/jenkinsSonar.groovy
+++ vars/jenkinsSonar.groovy
@@ -35,7 +35,7 @@ bat 'mvn test'
  stage("Quality Gate") {
             steps {
               timeout(time: 1, unit: 'MINUTES') {
-                waitForQualityGate abortPipeline: true
+                waitForQualityGate abortPipeline: false
               }
             }
  }
